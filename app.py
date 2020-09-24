82c8dcad6e0e4c1c7d01a42723997497e506a70f
Sarah Gonzalez Blanco <segonzalezblanco1@buffs.wtamu.edu>
2020-09-23 20:33:22 -0500

    Added Chapter 3.2 Conditional Statements

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 # Chapter 3.1 Introduction, no code
 # Chapter 3.2 Conditional Statements
+print("Chapter 3.2 Conditional Statements")
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+
+else:
+    print("It's cold")
+print("Done")
+print("*" * 30)

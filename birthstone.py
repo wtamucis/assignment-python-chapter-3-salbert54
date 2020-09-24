2ed7acd4187606d3dc653565d420640bfefa84c7
Sarah Gonzalez Blanco <segonzalezblanco1@buffs.wtamu.edu>
2020-09-23 23:12:32 -0500

    Completed task d and started task e.

--- birthstone.py
+++ birthstone.py
@@ -1,2 +1,8 @@
 # Follow the assignment instructions to code an app that
 # will tell a user their birthstone.
+user_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+input("Enter the number of the month you were born (1-12)")
+for user_input 1:
+    print("Your birthstone is garnet.")
+else:
+    print("Your birthstone is amethyst.")

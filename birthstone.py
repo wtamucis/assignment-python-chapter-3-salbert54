411f650e13f487e5a286eb33da6fa7db989240d6
Sarah Gonzalez Blanco <segonzalezblanco1@buffs.wtamu.edu>
2020-09-25 15:32:27 -0500

    Ready for grading

--- birthstone.py
+++ birthstone.py
@@ -1,8 +1,31 @@
 # Follow the assignment instructions to code an app that
 # will tell a user their birthstone.
-user_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-input("Enter the number of the month you were born (1-12)")
-for user_input 1:
+
+user_input = input("Enter the number of the month you were born (1-12) ")
+month = int(user_input)
+if month == 1:
     print("Your birthstone is garnet.")
-else:
+elif month == 2:
     print("Your birthstone is amethyst.")
+elif month == 3:
+    print("Your birthstone is bloodstone.")
+elif month == 4:
+    print("Your birthstone is diamond.")
+elif month == 5:
+    print("Your birthstone is emerald.")
+elif month == 6:
+    print("Your birthstone is pearl.")
+elif month == 7:
+    print("Your birthstone is ruby.")
+elif month == 8:
+    print("Your birthstone is peridot.")
+elif month == 9:
+    print("Your birthstone is sapphire.")
+elif month == 10:
+    print("Your birthstone is opal.")
+elif month == 11:
+    print("Your birthstone is topaz.")
+elif month == 12:
+    print("Your birthstone is turquoise.")
+else:
+    print("You made an invalid entry.")
